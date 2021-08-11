bc2f2a0355ee4efbc012bb41519436850556a451
JackSun <1716118719@qq.com>
2021-08-11 23:20:16 +0800

    Auto Update

--- gh-pages-autodeploy.sh
+++ gh-pages-autodeploy.sh
@@ -11,6 +11,6 @@ echo "------4/4 Push to github gh-pages branch------"
 git add .
 git commit -m "Auto Deployed."
 git remote add origin git@github.com:Happievening/webpack-demo.git
-git push origin gh-pages
+git pull && push origin gh-pages
 echo "------Done. Switch back to master branch.------"
 git checkout master
