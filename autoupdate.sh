1ddf727df606c3547d2c45317fa6e0e4af2b5575
JackSun <1716118719@qq.com>
2021-08-11 22:38:54 +0800

    Auto Update

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "Auto Update"
-git remote add origin git@github.com:Happievening/happievening.github.io.git
+git remote add origin git@github.com:Happievening/webpack-demo.git
 git push origin master
--- gh-pages-autodeploy.sh
+++ gh-pages-autodeploy.sh
@@ -1,5 +1,15 @@
-echo "------1/6 git checkout------"
+echo "------1/3 git checkout------"
+git checkout master
+git branch -D gh-pages
 git checkout gh-pages
-echo "------2/6 Preparing files------"
-ls |grep -v dist|xargs rm -rf
+echo "------2/3 Preparing files------"
+ls |grep -v dist|xargs rm -r
+cp ./dist/* ./
+rm -r ./dist
+echo "------3/3 Push to github gh-pages branch------"
+git add .
+git commit -m "Auto Deployed."
+git remote add origin git@github.com:Happievening/webpack-demo.git
+git push origin gh-pages
 echo "------Done. Switch back to master branch.------"
+git checkout master
